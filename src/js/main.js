522059cf0aec88ef90b04d175b658865cbcc4937
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2024-11-22 17:46:21 +0200

    zz

--- src/js/main.js
+++ src/js/main.js
@@ -38,3 +38,10 @@
         });
     
     })(jQuery);
+
+function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+  }
+function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+  }
