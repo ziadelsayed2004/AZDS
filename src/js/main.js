789e88a7f465a7c0c7454e84366a1009d7ad202e
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2024-11-27 13:04:36 +0200

    new

--- src/js/main.js
+++ src/js/main.js
@@ -41,9 +41,11 @@
 
 function openNav() {
         document.getElementById("myNav").style.width = "100%";
+        document.getElementById("progress-wrap").style.visibility = "hidden";        
   }
 function closeNav() {
         document.getElementById("myNav").style.width = "0%";
+        document.getElementById("progress-wrap").style.visibility = "visible";        
   }
   document.querySelector('.overlay').addEventListener('wheel', function(event) {
     event.preventDefault();
