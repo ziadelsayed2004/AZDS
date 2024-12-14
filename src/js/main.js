01020eeda425c7c2d43016605f25c8f2a9d34585
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2024-12-14 12:41:36 +0200

    new

--- src/js/main.js
+++ src/js/main.js
@@ -181,7 +181,7 @@ document.addEventListener("scroll", () => {
 // Services btn
 document.getElementById('services-btn').addEventListener('click', function() {
   window.scrollBy({
-    top: window.innerHeight*0.8,
+    top: window.innerHeight*0.82,
     behavior: 'smooth'
   });
 });
