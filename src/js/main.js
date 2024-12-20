f4324511d9a9d8285f64c72c12392f1cc52ba2ee
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2024-12-20 13:01:29 +0200

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -181,13 +181,13 @@ document.addEventListener("scroll", () => {
 // Services btn
 document.getElementById('services-btn').addEventListener('click', function() {
   window.scrollBy({
-    top: window.innerHeight*0.95,
+    top: window.innerHeight*0.92,
     behavior: 'smooth'
   });
 });
 document.getElementById('portofolio-btn').addEventListener('click', function() {
   window.scrollBy({
-    top: window.innerHeight*1.95,
+    top: window.innerHeight*1.82,
     behavior: 'smooth'
   });
 });
