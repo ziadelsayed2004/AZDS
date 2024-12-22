90160751145856b5e579caeee8973edcd1a2782f
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2024-12-22 14:27:57 +0200

    new

--- src/js/main.js
+++ src/js/main.js
@@ -221,13 +221,13 @@ document.addEventListener("scroll", () => {
 // Services btn
 document.getElementById('services-btn').addEventListener('click', function() {
   window.scrollBy({
-    top: window.innerHeight*0.8,
+    top: window.innerHeight*0.865,
     behavior: 'smooth'
   });
 });
 document.getElementById('portofolio-btn').addEventListener('click', function() {
   window.scrollBy({
-    top: window.innerHeight*1.6,
+    top: window.innerHeight*1.8,
     behavior: 'smooth'
   });
 });
